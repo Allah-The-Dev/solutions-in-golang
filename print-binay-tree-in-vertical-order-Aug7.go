27e1aa46488e51949fdca1d17e2916a08a3bed93
Allah-The-Dev <pandeyneelkumar@gmail.com>
2020-08-08 12:27:54 +0530

    aug 7 with queue

--- print-binay-tree-in-vertical-order-Aug7.go
+++ print-binay-tree-in-vertical-order-Aug7.go
@@ -18,51 +18,44 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
+//TreeNodeWithHD ...
+type TreeNodeWithHD struct {
+	Node *TreeNode
+	HD   int
+}
+
 func getHDLevelOrderedMap(root *TreeNode, hdMap map[int][]int, hd int, hdMin, hdMax *int) {
 
 	if root == nil {
 		return
 	}
 
-	if hdLevelArr, ok := hdMap[hd]; ok {
-		for i, num := range hdLevelArr {
-			if len(hdLevelArr) == 1 {
-				if root.Val < num {
-					fmt.Println(hdLevelArr)
-					hdLevelArr = append(hdLevelArr, 0)
-					copy(hdLevelArr[1:], hdLevelArr[:])
-					hdLevelArr[0] = root.Val
-				} else {
-					hdLevelArr = append(hdLevelArr, 0)
-					copy(hdLevelArr[0:], hdLevelArr[:])
-					hdLevelArr[1] = root.Val
-				}
-			} else {
-				if root.Val < num {
-					hdLevelArr = append(hdLevelArr, 0)
-					copy(hdLevelArr[i+1:], hdLevelArr[i:])
-					hdLevelArr[i] = root.Val
-				}
-				if root.Val > num && i == len(hdLevelArr)-1 {
-					hdLevelArr = append(hdLevelArr, root.Val)
-				}
-			}
+	queue := []*TreeNodeWithHD{{root, 0}}
+
+	for len(queue) != 0 {
+		node := queue[0]
+		queue = queue[1:]
+		//update map
+		if nodeDataArr, ok := hdMap[node.HD]; ok {
+			nodeDataArr = append(nodeDataArr, node.Node.Val)
+			hdMap[node.HD] = nodeDataArr
+		} else {
+			nodeDataArr = []int{node.Node.Val}
+			hdMap[node.HD] = nodeDataArr
 		}
-		hdMap[hd] = hdLevelArr
-	} else {
-		newHDLevelArr := []int{root.Val}
-		hdMap[hd] = newHDLevelArr
+		if *hdMin > node.HD {
+			*hdMin = node.HD
+		}
+		if *hdMax < node.HD {
+			*hdMax = node.HD
+		}
+		if node.Node.Left != nil {
+			queue = append(queue, &TreeNodeWithHD{node.Node.Left, node.HD - 1})
+		}
+		if node.Node.Right != nil {
+			queue = append(queue, &TreeNodeWithHD{node.Node.Right, node.HD + 1})
+		}
 	}
-
-	if *hdMin > hd {
-		*hdMin = hd
-	}
-	if *hdMax < hd {
-		*hdMax = hd
-	}
-
-	getHDLevelOrderedMap(root.Left, hdMap, hd-1, hdMin, hdMax)
-	getHDLevelOrderedMap(root.Right, hdMap, hd+1, hdMin, hdMax)
 
 }
 
